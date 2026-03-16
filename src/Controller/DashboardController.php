31ff3bd29f7b7f8653e390c3bcb04804019e6cc4
Nikoloz <nikoloz.mamyan@lnstrade.fr>
2026-03-16 12:18:57 +0100

    ConnectorFlux + services

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\Connector\ConnectorFluxManager;
 use App\Service\HubSpot\HubSpotClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,8 +17,18 @@ final class DashboardController extends AbstractController
     }
 
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(ConnectorFluxManager $connectorFluxManager): Response
     {
+        $flux = $connectorFluxManager->create(
+            'Produits Sage vers HubSpot',
+            '2xsfrezgeregth',
+            'sage',
+            'hubspot',
+            'article',
+            'product',
+            true,
+            'Flux de synchronisation des produits Sage vers HubSpot'
+            );
 
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
